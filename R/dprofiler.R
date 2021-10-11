7b9ec52c0b789042cbe470990320090342a6d556
Artur <artur@macbook-pro.ad.umassmed.edu>
2021-10-10 20:21:53 -0400

    fix global

--- R/dprofiler.R
+++ R/dprofiler.R
@@ -10,4 +10,4 @@ NULL
 utils::globalVariables(c("Conds", "ExpressionSet", "Samples","Scores", 'dc', 'demodata', 'demoprofdata',
                          'demoscdata', 'exprs', 'fData', 'grid.draw', 'grid.newpage', 'nnls', 'pData', 'pData<-',
                            'profileConds', 'silhouette', 'x', 'y',"waiting_screen","CellType","nCount_integratedRNA_norm"),
-                       add = FALSE)
+                       package = "dprofiler", add = FALSE)
