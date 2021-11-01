34b961c3f5d866ee9bac89d0f50556bbaf6588a5
Artur-man <artur@macbook-pro.ad.umassmed.edu>
2021-11-01 15:40:12 -0400

    fix global variables

--- R/dprofiler.R
+++ R/dprofiler.R
@@ -10,6 +10,6 @@ NULL
 utils::globalVariables(c("Conds", "ExpressionSet", "Samples","Scores", 'dc', 'demodata', 'demoprofdata',
                          'demoscdata', 'exprs', 'fData', 'grid.draw', 'grid.newpage', 'nnls', 'pData', 'pData<-',
                            'profileConds', 'silhouette', 'x', 'y',"CellType","nCount_integratedRNA_norm","Condition",
-                         "DGEList","calcNormFactors"),
+                         "DGEList","calcNormFactors","demovitiligoscdata"),
                        package = "dprofiler", add = FALSE)
 # utils::globalVariables(c("."))
